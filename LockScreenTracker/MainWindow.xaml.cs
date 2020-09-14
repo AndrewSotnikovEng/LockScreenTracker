8fdb9e5494da815ba8b47bb1863e046bd553aff9
Andrew Sotnikov <andrew.sotnikov@zoho.com>
2020-09-14 23:18:44 +0300

    Initial commit

---
+++ LockScreenTracker/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LockScreenTracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        DateTime lockTime;
+        DateTime unlockTime;
+        string diff;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            this.Loaded += MainWindow_Loaded;
+            Microsoft.Win32.SystemEvents.SessionSwitch += new Microsoft.Win32.SessionSwitchEventHandler(SystemEvents_SessionSwitch);
+
+        }
+
+        public string Diff { get => diff; set {
+                diff = value;
+                OnPropertyChanged();
+            } }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public void SystemEvents_SessionSwitch(object sender, Microsoft.Win32.SessionSwitchEventArgs e)
+        {
+
+            if (e.Reason == SessionSwitchReason.SessionLock)
+            {
+                //I left my desk
+                lockTime = DateTime.Now.ToUniversalTime();
+                
+            }
+            else if (e.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                unlockTime = DateTime.Now.ToUniversalTime();
+                Console.WriteLine($"I returned to my desk{DateTime.Now}");
+                Diff = (unlockTime - lockTime).ToString(@"hh\:mm\:ss");
+
+            }
+        }
+
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
+
